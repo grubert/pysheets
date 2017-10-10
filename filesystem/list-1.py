15b23eb5ed24f3d20260762d1557b89135400c00
engelbert gruber <engelbert.gruber@gmail.com>
2017-10-10 21:20:48 +0200

    Verzeichnisinhalt auflisten

---
+++ filesystem/list-1.py
@@ -0,0 +1,27 @@
+"""
+Verzeichnis durch...laufen und ausgeben
+"""
+
+# unser test Verzeichnis
+DIR="testdir"
+
+print("mit prozeduralem Modul: os")
+import os
+
+print(os.listdir(DIR))
+
+print("\nmit objektorientiertem Modul: pathlib")
+import pathlib
+
+# ein Objekt vom Erzeugen 
+dir = pathlib.Path(DIR)
+# und alle Einträge ausgeben
+for entry in dir.iterdir():
+    print(entry)
+
+# wenn man nur schnell den Inhalt sehen will ist os.listdir schneller
+# wenn es formattiert sein soll
+print("\nnoch einmal mit prozeduralem Modul: os")
+for entry in os.listdir(DIR):
+    print(entry)
+# ist nicht mehr viel Unterschied
